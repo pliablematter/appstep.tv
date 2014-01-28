f988bd5fcc11151a310a2696a68ab252c57ad41b
Doug Burns <doug@pliablematter.com>
2014-01-28 05:15:23 -0500

    03-03 Added table view controller and table view cell for saved view. Connected to storyboard.

---
+++ stopwatch/stopwatch/SavedTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  SavedTableViewController.h
+//  stopwatch
+//
+//  Created by Doug Burns on 1/28/14.
+//  Copyright (c) 2014 Pliable Matter LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SavedTableViewController : UITableViewController
+
+@end
